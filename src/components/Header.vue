435c375aa88be1e08b1aac9ea6a6870193be1bb3
nicolafelice <felicenicola99@hotmail.com>
2021-10-11 12:14:38 +0200

    add: header markup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,53 @@
 <template>
   <header>
+    <div class="logo_wrapper">
+      <img src="../assets/img/dark-logo.png" alt="logo">
+    </div>
 
+    <nav class="navigation">
+      <ul>
+        <li>
+          <a href="#">home</a>
+          <font-awesome-icon icon="chevron-down" />
+          <!-- HERE menu dropdown -->
+        </li>
+        <li>
+          <a href="#">home</a>
+          <font-awesome-icon icon="chevron-down" />
+          <!-- HERE menu dropdown -->
+        </li>
+        <li>
+          <a href="#">home</a>
+          <font-awesome-icon icon="chevron-down" />
+          <!-- HERE menu dropdown -->
+        </li>
+        <li>
+          <a href="#">home</a>
+          <font-awesome-icon icon="chevron-down" />
+          <!-- HERE menu dropdown -->
+        </li>
+        <li>
+          <a href="#">home</a>
+          <font-awesome-icon icon="chevron-down" />
+          <!-- HERE menu dropdown -->
+        </li>
+        <li>
+          <a href="#">home</a>
+          <font-awesome-icon icon="chevron-down" />
+          <!-- HERE menu dropdown -->
+        </li>
+      </ul>
+    </nav>
+
+    <div class="user_profile_icon">
+      <a href="#">
+        <font-awesome-icon :icon="['far', 'user']" />
+      </a>
+    </div>
+
+    <div class="search_bar">
+      <input type="text" placeholder="search...">
+    </div>
   </header>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faChevronDown, faUser);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false
 
 new Vue({
