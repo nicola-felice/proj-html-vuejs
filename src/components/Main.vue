3bd221d9001ece47175765e131dd51631889f582
nicolafelice <felicenicola99@hotmail.com>
2021-10-14 01:27:18 +0200

    add: CTA section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,6 +8,8 @@
 
     <Reviews-section :reviews="reviewsList" />
 
+    <CTA />
+
     <div style="height: 50vh; background-color: white;" id="spacing"></div>
   </main>
 </template>
@@ -18,6 +20,7 @@ import HeroSection from './sections/Hero.vue';
 import OnlineCoursesSection from './sections/OnlineCoursesSection.vue';
 import SmartTutoringSection from './sections/SmartTutoringSection.vue';
 import ReviewsSection from './sections/ReviewsSection.vue';
+import CTA from './sections/CTA.vue';
 // json
 import reviewsJson from '../assets/json/reviews.json';
 
@@ -29,6 +32,7 @@ export default {
     OnlineCoursesSection,
     SmartTutoringSection,
     ReviewsSection,
+    CTA,
   },
   data() {
     return {
---
+++ src/components/sections/CTA.vue
@@ -0,0 +1,64 @@
+<template>
+  <section>
+    <div class="container">
+      <h3 class="section_title">Start today for getting <em>Online Certification</em></h3>
+      <h2 class="section_title">You can be your own guiding star with our help!</h2>
+      <button class="btn">Get started now</button>     
+
+      <img class="pattern shape_1" src="../../assets/img/shapes/maxcoach-shape-01.png" alt="">
+      <img class="pattern shape_2" src="../../assets/img/shapes/maxcoach-shape-02.png" alt="">
+      <div class="pattern shape_3"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CTA',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/vars.scss';
+
+section .container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 5rem;
+  position: relative;
+  h3.section_title {
+    font-size: 1.25rem;
+    margin-bottom: .4rem;
+  }
+  h2.section_title {
+    margin-bottom: 2rem;
+  }
+}
+
+.pattern {
+  position: absolute;
+  z-index: 0;
+}
+.shape_1 {
+  left: 3%;
+  top: 35%;
+  transform: translateY(-50%);
+  width: 7rem;
+}
+.shape_2 {
+  right: 2%;
+  bottom: 35%;
+  width: 6rem;
+}
+.shape_3 {
+  border-radius: 50%;
+  border: 7px solid $main-pink;
+  opacity: .7;
+  width: 45px;
+  height: 45px;
+
+  top: 8%;
+  left: 2%;
+}
+</style>
